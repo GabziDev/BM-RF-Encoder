7ffee7774f9f1c36f792e03c9dbb219579c1d4cd
GabziDev <ggabz338972007@gmail.com>
2025-03-15 22:54:14 +0100

    Update morse.h, fix textToMorse + add ASCII->binary converter

--- src/main.c
+++ src/main.c
@@ -21,42 +21,70 @@ void morseReadWIN(const char* code, size_t codeSize) {
 }
 
 // Convert text to morse
-void textToMorse(const char* text, size_t textSize) {
-    char* morseText = malloc(textSize * 5 * sizeof(char)); // 5 caractères max code morse
-
-    morseText[0] = '\0';
+void textToMorse(const char *text, size_t textSize, char **morse) {
+    *morse = malloc(textSize * 5 * sizeof(char)); // 5 caractères max code morse
+    *morse[0] = '\0';
 
     for (int i = 0; i < textSize; i++) {
         if (text[i] == ' ') {
-            strcat(morseText, " ");
+            strcat(*morse, " ");
             continue;
         }
 
         for (int j = 0; j < MORSE_TABLE_SIZE; j++) {
             char currentChar = text[i];
 
-            if (toupper(currentChar) == morseTable[j].lettre) {
+            if (toupper(currentChar) == morseTable[j].c) {
                 #if __WIN32__
                     size_t codeSize = strlen(morseTable[j].code);
                     morseReadWIN(morseTable[j].code, codeSize); // test
                 #endif
-
-                strcat(morseText, morseTable[j].code);
-                strcat(morseText, " ");
+                strcat(*morse, morseTable[j].code);
+                strcat(*morse, "/");
                 break;
             }
         }
     }
+}
 
-    printf("\n\nFinal : %s\n", morseText);
-    free(morseText);
+// convert decimal to binary
+void decimalToBinary(int decimal, char *octet) {
+    // init octet avec 8 zero
+    for (int i = 7; i >= 0; i--) {
+        octet[i] = (decimal % 2) + '0';
+        decimal /= 2;
+    }
+
+    octet[8] = '\0';
+}
+
+// convert text to binary
+void textToBinary(const char *text, size_t textSize, char **binary) {
+    *binary = malloc((textSize * 9 + 1) * sizeof(char));
+    *binary[0] = '\0';
+
+    char octet[9];
+    for (size_t i = 0; i < textSize; i++) {
+        decimalToBinary((unsigned char)text[i], octet);  // convertir chaque char en binaire
+        strcat(*binary, octet);
+    }
 }
 
 int main(void) {
-    char* text = "Hello world";
+    char *text = "Hello world";
     size_t textSize = strlen(text);
 
-    textToMorse(text, textSize);
+    char *morse = NULL;
+    char *binary = NULL;
+
+    textToMorse(text, textSize, &morse);
+    printf("\n\nMorse : %s\n", morse);
+
+    textToBinary(text, textSize, &binary);
+    printf("\n\nBinary : %s\n", binary);
+
+    free(morse);
+    free(binary);
 
     return 0;
 }
